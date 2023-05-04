104cc2262794cedb1114484d631062c795614bb5
YuraML <malashtayuriy@gmail.com>
2023-05-04 19:06:56 +0400

    Upload GeoJSON

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,30 +1,27 @@
 from django.shortcuts import render
+from places.models import Place
 
 
 def show_main_page(request):
+    features = []
+    places = Place.objects.all()
+
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [place.lng, place.lat]},
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": '-',
+            },
+        }
+        features.append(feature)
+
     context = {
         "places": {
             "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "geometry": {"type": "Point", "coordinates": [37.62, 55.793676]},
-                    "properties": {
-                        "title": "«Легенды Москвы",
-                        "placeId": "moscow_legends",
-                        "detailsUrl": "static/places/moscow_legends.json",
-                    },
-                },
-                {
-                    "type": "Feature",
-                    "geometry": {"type": "Point", "coordinates": [37.64, 55.753676]},
-                    "properties": {
-                        "title": "Крыши24.рф",
-                        "placeId": "roofs24",
-                        "detailsUrl": "static/places/roofs24.json",
-                    },
-                },
-            ],
+            "features": features
         }
     }
     return render(request, "index.html", context)
