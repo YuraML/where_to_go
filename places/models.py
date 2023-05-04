b86fdb8fe2d1258853896b78eb79a78be98ee4ad
YuraML <malashtayuriy@gmail.com>
2023-05-04 17:03:20 +0400

    Add model for images

--- places/models.py
+++ places/models.py
@@ -10,3 +10,12 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', null=True)
+    img = models.ImageField()
+    number = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.number} {self.place.title if self.place else 'Image'}"
