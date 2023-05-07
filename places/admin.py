01f7479e91ad9dc6ffbe79462af061f938cb9d40
YuraML <malashtayuriy@gmail.com>
2023-05-07 19:34:31 +0400

    make photo upload easier

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Place, Image
 
 
-admin.site.register(Place)
+class ImageInline(admin.TabularInline):
+    model = Image
+    raw_id_fields = ("place", )
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ["title"]
+    inlines = [ImageInline]
+
 admin.site.register(Image)
