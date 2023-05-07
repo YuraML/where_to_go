de119d0f5a9a581f1d8a70c4b652c0e5936b1589
YuraML <malashtayuriy@gmail.com>
2023-05-07 20:49:20 +0400

    add WYSIWYG

--- places/models.py
+++ places/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.TextField()
-    description_long = models.TextField()
+    description_long = HTMLField()
     lng = models.FloatField()
     lat = models.FloatField()
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .views import show_main_page, get_place_specs
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', show_main_page),
-    path('places/<int:place_id>/', get_place_specs, name="place_specs"),
+    path('places/<int:place_id>/', get_place_specs, name='place_specs'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
